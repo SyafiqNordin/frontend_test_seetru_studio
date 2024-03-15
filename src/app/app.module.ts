61cf5ec15ba7465c6580548e8c633316ceb79c29
Syxfqn <msyaf0911@gmail.com>
2024-03-15 11:56:22 +0800

    Remove components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EstablishmentComponent } from './pages/establishment/establishment.component';
-import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EstablishmentComponent,
-    ContactUsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
